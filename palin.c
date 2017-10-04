922e7d84c23c6798b620f2867791523f0e538423
jcldyc <jcldyc@mail.umsl.edu>
2017-10-04 05:08:51 -0500

    m

--- palin.c
+++ palin.c
@@ -101,12 +101,16 @@ int main(int argc, char *argv[]){
 			//printTime();
             fprintf(stderr, "\t| %s | \t process: %d\t | Trying to enter Critical Section |\n",buffer, pNum);
 
-            shmPtr->flag[pNum] = want_in;
-            j = shmPtr->turn; 
+            shmPtr->flag[pNum] = want_in; // Raise my flag
+            j = shmPtr->turn; // Set local variable
             while ( j != pNum )                                            
-                j = ( shmPtr->flag[j] != idle ) ? shmPtr->turn( j + 1 ) % n;      
+                j = ( shmPtr->flag[j] != idle ) ? shmPtr->turn : ( j + 1 ) % n;         //if the current process whose turn it is idle set j to (j+1) %n
+
+            // Declare intention to enter critical section
 
             shmPtr->flag[pNum] = in_cs;
+
+            // Check that no one else is in critical section
 
             for ( j = 0; j < n; j++ ){
                 if ( ( j != pNum ) && ( shmPtr->flag[j] == in_cs ) ){
@@ -114,6 +118,8 @@ int main(int argc, char *argv[]){
                 }
             }
         } while (( j < n ) || ( shmPtr->turn != pNum && shmPtr->flag[shmPtr->turn] != idle ));
+
+        // Assign turn to self and enter critical section
 
         shmPtr->turn = pNum;      
 
@@ -181,15 +187,6 @@ int main(int argc, char *argv[]){
 
 const int isPalindrome(char *str){
     // Start from leftmost and rightmost corners of str
-	
-	int length = strlen(str);
-	for(int i=0;i<length;i++){
-		if(isalnum(str[i])){
-			str[i]=tolower(str[i]);
-		}
-		
-	}
-	
     int l = 0;
     int h = strlen(str) - 1;
  
@@ -206,9 +203,25 @@ const int isPalindrome(char *str){
 
  
  
+ /*
+ * Function to check whether a string is palindrome or not
+ */
  
  
- 
+/*  const int isPalindrome(char *inputString, int leftIndex, int rightIndex){
+     
+     if(NULL == inputString || leftIndex < 0 || rightIndex < 0){
+         printf("Invalid Input");
+         return 0;
+     }
+     
+     if(leftIndex >= rightIndex)
+         return 1;
+     if(inputString[leftIndex] == inputString[rightIndex]){
+         return isPalindrome(inputString, leftIndex + 1, rightIndex - 1);
+     }
+     return 0;
+ } */
  
  /* const int isPalindrome(char * palindromeString){
 
