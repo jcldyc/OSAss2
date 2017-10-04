adf0939b656db74c6b6c60255251b0d12ac1da5e
jcldyc <jcldyc@mail.umsl.edu>
2017-10-03 22:29:13 -0500

    m

--- palin.c
+++ palin.c
@@ -211,7 +211,7 @@ int main(int argc, char *argv[]){
 }
 
 
-const int isPalindrome(char * pString){					//Palindrome website reference: http://www.geeksforgeeks.org/c-program-check-given-string-palindrome/
+/* const int isPalindrome(char * pString){					//Palindrome website reference: http://www.geeksforgeeks.org/c-program-check-given-string-palindrome/
 
   // Start from leftmost and rightmost corners of str
     int l = 0;
@@ -228,6 +228,25 @@ const int isPalindrome(char * pString){					//Palindrome website reference: http
     return 1;	
 
 
+} */
+
+	const int isPalindrome(char *str)
+{
+    // Start from leftmost and rightmost corners of str
+    int l = 0;
+    int h = strlen(str) - 1;
+ 
+    // Keep comparing characters while they are same
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+           // printf("%s is Not Palindromen", str);
+            return 0;
+        }
+    }
+    //printf("%s is palindromen", str);
+	return 1;
 }
 
 
