9b3d4526690c18d513e49c9101896e24727a5a5a
jcldyc <jcldyc@mail.umsl.edu>
2017-10-04 02:26:06 -0500

    m

--- palin.c
+++ palin.c
@@ -90,15 +90,11 @@ int main(int argc, char *argv[]){
         }
     
         do{
-            /* time(&timer);
+            time(&timer);
             tm_info = localtime(&timer);
-            strftime(wantInTime, 26, "%H:%M:%S", tm_info); */
-			time_t now;
-			time(&now);
- 
-			printf("%s", ctime(&now));
+            strftime(wantInTime, 26, "%H:%M:%S", tm_info);
 			
-            fprintf(stderr, "\t%s\tprocess: %d\twants to enter the critical section.\n", ctime(&now), procNum);
+            fprintf(stderr, "\t%s\tprocess: %d\twants to enter the critical section.\n", wantInTime, procNum);
 
             shmPtr->flag[procNum] = want_in; // Raise my flag
             j = shmPtr->turn; // Set local variable
